2679d4b8d475ddaeee967ef0fe56d17c8799a79c
AndreyOksenyuk <oksenyuk.andrey@gmail.com>
2020-09-06 15:57:40 +0300

    Updates

---
+++ precache-manifest.d391e6f8a0e32a5e5add991a17d03741.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942ab1f9cab460bf187eb9ff5197c34e",
+    "url": "/test-task-forma/index.html"
+  },
+  {
+    "revision": "63cf0b7ee8db8809bd92",
+    "url": "/test-task-forma/static/css/main.a7a3e1ca.chunk.css"
+  },
+  {
+    "revision": "6962509b032dd355bad1",
+    "url": "/test-task-forma/static/js/2.090cabb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task-forma/static/js/2.090cabb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63cf0b7ee8db8809bd92",
+    "url": "/test-task-forma/static/js/main.ae7bfca1.chunk.js"
+  },
+  {
+    "revision": "9b6287ef19e55d10bd86",
+    "url": "/test-task-forma/static/js/runtime-main.390f6b5c.js"
+  },
+  {
+    "revision": "98bf746b14deb46cfe67a37695c39796",
+    "url": "/test-task-forma/static/media/Folder.98bf746b.svg"
+  },
+  {
+    "revision": "30f6810efd811f0b9454dfae7c4e4e14",
+    "url": "/test-task-forma/static/media/error.30f6810e.svg"
+  },
+  {
+    "revision": "f097712262e23f795b786df848b3bb81",
+    "url": "/test-task-forma/static/media/success.f0977122.svg"
+  },
+  {
+    "revision": "a4c1d7a149f622dbbb7ded74b4c08b20",
+    "url": "/test-task-forma/static/media/warning.a4c1d7a1.svg"
+  }
+]);
